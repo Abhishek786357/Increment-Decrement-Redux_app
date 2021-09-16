1736068baefda7b686fbeb19d809416341cad8a5
Abhishek Chanalia <abhijhansa@gmail.com>
2021-09-16 12:11:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {useSelector , useDispatch} from "react-redux";
+import {incNumber, decNumber} from "./actions/index"
+
+ const App=()=>{
+const myState = useSelector((state) =>state.changeTheNumber)
+const  dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  {/* <div className="main-div">
+          <div className="center_div">
+              <h1>0</h1>
+              <div className="btn_div"> 
+              <button value ={myState}>Increment</button>
+              <button>Decrement</button>
+              </div>
+          </div>
+
+      </div> */}
+<div> <h1>Hello Redux</h1>
+<div>
+  <a onClick={()=> dispatch (decNumber())}><button><span>- </span></button></a>
+  <input  value={myState}></input>
+  <a onClick={()=> dispatch (incNumber())} ><button><span>+ </span></button></a>
+</div>
+</div>
+    </>
   );
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import changeTheNumber from "./upDown";
+import {combineReducers} from 'redux';
+
+
+// complete State
+const rootReducer= combineReducers({
+    changeTheNumber
+
+});
+
+export default rootReducer;
